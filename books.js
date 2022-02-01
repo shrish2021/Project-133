2967f13c43364e12c3ef39931f87e9ff83de634f
shrish2021 <77865484+shrish2021@users.noreply.github.com>
2022-01-31 19:48:47 -0500

    Add files via upload

---
+++ books.js
@@ -0,0 +1,40 @@
+status = "";
+objects = [];
+
+function preload()
+{
+    books = loadImage("books.jpeg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResults(results)
+{
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    if(results.length > 0)
+    {
+        for(i=0; i < results.length; i++)
+        {
+            percentage = floor(results[0].confidence * 100);
+            text(objects[i] + "" + percentage + "%", objects[i] + 15, objects[i] + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
